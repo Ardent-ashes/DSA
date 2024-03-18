55076ea22fa61507d80634ca63b1ae2813677f73
Ardent-ashes <128883807+Ardent-ashes@users.noreply.github.com>
2024-03-18 22:40:08 +0600

    Create wordmaking.cpp

---
+++ wordmaking.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printStack(stack<char> s) {
+    while (!s.empty()) {
+        std::cout << s.top() << " ";
+        s.pop();
+    }
+   cout <<endl;
+}
+stack<char> copyStack(const stack<char>& original) {
+    stack<char> copiedStack;
+    stack<char> tempStack;
+
+    tempStack = original;
+    while (!tempStack.empty()) {
+        copiedStack.push(tempStack.top());
+        tempStack.pop();
+    }
+  // Return the copied stack
+    return copiedStack;
+}
+
+void generate(stack<char>s, string word)
+{
+
+    stack<char>cp;
+    if(s.size()==word.length())
+    {
+        printStack(s);
+    }
+    cp=copyStack(s);
+    for(int i=0;i<word.length();i++)
+    {
+        cp.push(word[i]);
+        generate(cp,word);
+    }
+}
+
+int main()
+{
+    string s="abc";
+    stack<char>e;
+    generate(e,s);
+}
