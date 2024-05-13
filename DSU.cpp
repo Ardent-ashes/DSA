32dc8f295d218a2f625373be9c2409514189f46c
Ardent-ashes <128883807+Ardent-ashes@users.noreply.github.com>
2024-05-13 23:35:31 +0600

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h> 
+#include <cstdlib>
+using namespace std;
+
+int parent[100];
+int ranks[100];
+
+void make_set(int v) {
+    parent[v] = v;
+    ranks[v] = 0;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (ranks[a] < ranks[b])
+            swap(a, b);
+        parent[b] = a;
+        if (ranks[a] == ranks[b])
+            ranks[a]++;
+    }
+}
+
+int main() { 
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int y;
+        cin >> y;
+        make_set(y);
+    }
+
+    int u;
+    cin >> u;
+    for (int i = 0; i < u; i++) {
+        int x, y;
+        cin >> x >> y;
+        union_sets(x, y);
+    }
+    
+    int f;
+    cin >> f;
+    for (int i = 0; i < f; i++) {
+        int x;
+        cin >> x;
+        cout << find_set(x) << "\n";
+    }
+  
+    return 0; 
+}
