702611aaf6614cf6d49fe49f2dc89bedb5b9ecc8
Ardent-ashes <128883807+Ardent-ashes@users.noreply.github.com>
2024-05-13 23:38:18 +0600

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -10,6 +10,22 @@ void make_set(int v) {
     ranks[v] = 0;
 }
 
+/*void make_set(int v) {
+    parent[v] = v;
+    size[v] = 1;
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (size[a] < size[b])
+            swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+    }
+}*/
+
 int find_set(int v) {
     if (v == parent[v])
         return v;
